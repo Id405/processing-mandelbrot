60ee481bb84d8fa3bca8e2250b3a03bc3f3311f5
Id405 <8o7wermobile@gmail.com>
2019-11-06 08:31:12 -0900

    Hehehe :3 the orbit has now been trapped x3
    Uwu

--- OrbitTrapping.glsl
+++ OrbitTrapping.glsl
@@ -5,12 +5,27 @@ uniform float iterations;
 uniform vec2 transl;
 uniform dvec2 trap;
 
+uniform float contrast;
+uniform vec2 g1;
+uniform vec2 g2;
+
+vec2 map(vec2 value, vec2 inMin, vec2 inMax, vec2 outMin, vec2 outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+//https://github.com/hughsk/glsl-hsv2rgb/blob/master/index.glsl
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 dvec2 multComplex(dvec2 p1, dvec2 p2) {
     return dvec2(p1.x * p2.x - p1.y * p2.y, p1.x * p2.y + p1.y * p2.x);
 }
 
-vec4 iterate(dvec2 c) {
-    float distance = 100000000;
+vec3 iterate(dvec2 c) {
+    float minPointTrap = 100000000;
     
     float bailout = 2.0;
     dvec2 z = vec2(0);
@@ -22,25 +37,25 @@ vec4 iterate(dvec2 c) {
             break;
         }
 	
-	double zMinusPoint = length(z - trap);
-	if(zMinusPoint < distance) {
-	    distance = float(zMinusPoint);
+	double pointTrap = length(z - trap);
+	if(pointTrap < minPointTrap) {
+	    minPointTrap = float(pointTrap);
 	}
     }
     
-    //float gray = i/iterations;
-    float gray = distance/10.0;
+    float value = i/iterations;
+    vec2 gradient = map(min(vec2(minPointTrap*contrast), 1.0), vec2(0.0), vec2(1.0), g1, g2);
     
-    return vec4(gray, gray, gray, 1.0);
+    return hsv2rgb(vec3(gradient, value));
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
-    dvec2 c = (dvec2(fragCoord) - dvec2(iResolution.xy)/2.0)/dvec2(iResolution.x);
+    dvec2 c = (fragCoord - iResolution.xy/2.0)/iResolution.x;
     
     c *= dvec2(scale);
     c += dvec2(transl);
     
-    fragColor = iterate(c);
+    fragColor = vec4(iterate(c), 1.0);
 }
 
 void main() {
